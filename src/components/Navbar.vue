238cab9252ec4ce007d47bc3d23235b416e5c985
KaliAsha <me@kaliasha.xyz>
2018-06-11 20:08:58 +0200

    Add Clock on Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,6 +9,30 @@
             <b-navbar-nav>
                 <b-nav-item href="#">Link</b-nav-item>
             </b-navbar-nav>
+
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item @click.stop="changeDisplayClock"><Clock :long="clockLongDisplay"/></b-nav-item>
+            </b-navbar-nav>
         </b-collapse>
     </b-navbar>
 </template>
+
+<script>
+import Clock from './Widgets/Clock'
+
+export default {
+  components: {
+    Clock
+  },
+  data () {
+    return {
+      clockLongDisplay: false
+    }
+  },
+  methods: {
+    changeDisplayClock () {
+      this.clockLongDisplay = !this.clockLongDisplay
+    }
+  }
+}
+</script>
---
+++ src/components/Widgets/Clock.vue
@@ -0,0 +1,32 @@
+<template>
+    <span class="widget-clock">{{ clock.substring(0, long ? 8 : 5) }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    long: Boolean
+  },
+  data () {
+    return {
+      time: new Date(),
+      longMode: this.long
+    }
+  },
+  computed: {
+    clock () {
+      return this.time.toTimeString()
+    }
+  },
+  mounted () {
+    this.startClock()
+  },
+  methods: {
+    startClock () {
+      setInterval(() => {
+        this.time = new Date()
+      }, 1000)
+    }
+  }
+}
+</script>
